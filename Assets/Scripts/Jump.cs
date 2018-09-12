7decb97f65283ee99ed3e33bbdb62f39df082bd2
Brandon <brandonwstiles@gmail.com>
2018-09-11 22:01:12 -0500

    Changed to SideScroller

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+
+    public bool isGrounded;
+    Rigidbody2D rbody;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //if (rbody.velocity.y < 0)
+            //rbody.velocity = new Vector2(0, -jumpForce * 1.5f *Time.deltaTime);
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+                print("JUMP!!!");
+            rbody.velocity = new Vector2(0, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+            isGrounded = false;
+    }
+}
