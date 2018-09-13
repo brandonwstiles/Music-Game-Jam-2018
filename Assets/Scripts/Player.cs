a18e4e03d717ce00bd075816d028ad9cd6519c63
Brandon <brandonwstiles@gmail.com>
2018-09-13 17:27:35 -0500

    Rebuilt Repo with .gitignore

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveDistance;
+    public float moveSpeed;
+    public bool isDead;
+
+    Vector2 targetPos;
+    
+    private void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    void Update ()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed);
+
+        if (targetPos.x % moveDistance != 0)
+            targetPos.x = 0;
+
+        if (Input.GetKeyDown(KeyCode.A) && targetPos.x > -moveDistance * 2)
+        {
+            targetPos = new Vector2(transform.position.x - moveDistance, transform.position.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && targetPos.x < moveDistance * 2)
+        {
+            targetPos = new Vector2(transform.position.x + moveDistance, transform.position.y);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Run.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour {
+
+    public float runSpeed  = 10f;
+
+    Rigidbody2D rbody;
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+       
+    }
+
+    private void FixedUpdate()
+    {
+        rbody.velocity = new Vector2(runSpeed, rbody.velocity.y);
+    }
+}
