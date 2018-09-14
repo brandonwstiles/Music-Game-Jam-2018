73beebd87460f90c88465513f260000733e0fe8e
Brandon <brandonwstiles@gmail.com>
2018-09-14 09:09:15 -0500

    Cleaned Repo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+    
+    public AudioSource musicFile;
+
+    void Start()
+    {
+        if (!player)
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        musicFile = GetComponent<AudioSource>();
+        musicFile.Play(0);
+
+    }
+
+    void Update()
+    {
+       // if (player.GetComponent<Player>().isDead && Input.GetKey(KeyCode.Space))
+        //    SceneManager.LoadScene("SampleScene");
+    }
+}
